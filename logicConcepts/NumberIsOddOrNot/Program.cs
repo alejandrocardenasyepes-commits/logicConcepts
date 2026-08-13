b2463d34dcaa0cb5d727180a2bbd72ee06d1024b
Alejandro Cárdenas Yepes <alejandrocardenasyepes@gmail.com>
2026-08-13 15:17:13 -0500

    exercise 01 finish

---
+++ logicConcepts/NumberIsOddOrNot/Program.cs
@@ -0,0 +1,21 @@
+﻿do
+{
+    Console.Write("ingrese un numero o ingrese control + c para terminar:");
+    var numberString = Console.ReadLine();
+    try
+    {
+        var numberInt = int.Parse(numberString!);
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"el numero: {numberInt}, es par");
+        }
+        else
+        {
+            Console.WriteLine($"el numero: {numberInt}, es impar");
+        }
+    }
+    catch
+    {
+        Console.WriteLine($"el numero ingresado: {numberString}, no es valido. solo utilice caracteres numericos.");
+    }
+} while (true);
